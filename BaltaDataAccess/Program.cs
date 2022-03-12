8e99f38da50902c3c15c50c2ba88f5b28a12d60d
Lucas Cavichioli <lucascavica@gmail.com>
2022-03-12 16:32:48 -0300

    Creat CRUD

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -41,6 +41,27 @@ namespace BaltaDataAccess
         static void Dapper()
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+                       
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection);
+                ListaCategories(connection);
+                //CreateCategory(connection);
+
+            }
+        }
+
+
+        static void ListaCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+        static void CreateCategory(SqlConnection connection) 
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -59,30 +80,38 @@ namespace BaltaDataAccess
                                     @order, 
                                     @description, 
                                     @featured)";
-            
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var rows = connection.Execute(insertSql, new { 
-                                                    Id = category.Id, 
-                                                    title = category.Title,
-                                                    url = category.Url,
-                                                    description = category.Description,
-                                                    order = category.Order,
-                                                    summary = category.Summary,
-                                                    featured = category.Featured
 
-                                                  }
+            var rows = connection.Execute(insertSql, new
+            {
+                Id = category.Id,
+                title = category.Title,
+                url = category.Url,
+                description = category.Description,
+                order = category.Order,
+                summary = category.Summary,
+                featured = category.Featured
+
+            }
                                   );
 
-                Console.WriteLine($"{rows} linhas inseridas");
+            Console.WriteLine($"{rows} linhas inseridas");
 
+        }
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title] =@title WHERE [Id]=@id";
 
-                var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");      
-                }
-            }
+            var rows = connection.Execute(updateQuery, new { 
+                  id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                  title = "Front End 2021"
+            });
+
+            Console.WriteLine($"{rows} registros Atualizadas");
+
+        }
+        static void DeleteCategory() 
+        { 
+        
         }
     }
 }
